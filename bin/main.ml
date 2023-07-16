017d760b05bfd927ab2896f46b5a5ee3d2078283
henri <henri1i.dev@gmail.com>
2023-07-16 06:46:17 -0300

    feat: write a basic lexer

--- bin/main.ml
+++ bin/main.ml
@@ -1,26 +1,78 @@
-module Context = Map.Make(String)
+module Interpreter = struct 
+  module Context = Map.Make(String)
 
-type expr =
-  | Var of string
-  | Abs of (string * expr)
-  | App of (expr * expr)
+  type expr =
+    | Var of string
+    | Abs of (string * expr)
+    | App of (expr * expr)
 
-type value =
-  | Closure of { context: value Context.t; param : string; body : expr; }
+  type value =
+    | Closure of (string * expr * value Context.t)
 
-let rec eval context expr =
-  match expr with
-  | Var name -> Context.find name context
-  | Abs (param, body) -> Closure {context; param; body}
-  | App (l_expr, r_expr) ->
+  let rec eval context expr =
+    let apply context l_expr r_expr =
       let arg = eval context r_expr in
-      match eval context l_expr with
-      | Closure {context; param; body} ->
-          let context = Context.add param arg context in
-          eval context body
+        match eval context l_expr with
+        | Closure (param, body, context) ->
+            let context = Context.add param arg context in
+            eval context body in
 
-(** (\x.x x) (\x.x x) **)
-let u_comb = Abs ("x", App((Var "x"), (Var "x")))
-let ast = App(u_comb, u_comb)
+    match expr with
+    | Var name -> (match Context.mem name context with
+      | true -> Context.find name context
+      | false -> failwith ("unbound variable: " ^ name))
+    | Abs (param, body) -> Closure (param, body, context)
+    | App (l_expr, r_expr) -> apply context l_expr r_expr
+        
+  (** (\x.x x) (\x.x x) **)
+  let u_comb = Abs ("x", App((Var "x"), (Var "x")))
+  let ast = App(u_comb, u_comb)
 
-let _result = eval Context.empty ast
+  let _result = eval Context.empty ast
+end
+
+module Lexer = struct
+  type token =   
+  | LParen
+  | RParen
+  | Lambda
+  | Dot
+  | Identifier of string
+
+  let rec lex input =
+    let is_alpha = function 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false in
+
+    let rec lex_ident acc seq =
+      match seq with
+      | char::seq when is_alpha char -> lex_ident (acc ^ String.make 1 char) seq
+      | _ -> Identifier acc in
+
+    match input with
+    | [] -> []
+    | char :: seq ->
+        let rest = lex seq in
+        match char with
+        | '(' -> LParen::rest
+        | ')' -> RParen::rest
+        | '\\' -> Lambda::rest
+        | '.' -> Dot::rest
+        | char when is_alpha char ->
+            let token = lex_ident (String.make 1 char) seq in
+            token::rest
+        | _ -> rest
+
+  let lex input =
+    let chars = List.of_seq (String.to_seq input) in
+    lex chars
+
+  let debug tokens =
+    List.iter (fun token -> match token with
+      | LParen -> print_endline "("
+      | RParen -> print_endline ")"
+      | Lambda -> print_endline "λ"
+      | Dot -> print_endline "."
+      | Identifier id -> print_endline id) tokens
+end
+
+
+
